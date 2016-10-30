c45442eded0c817d1be2ccad2650fc613b234d04
Fred Mitchell <fred.mitchell@gmx.de>
2016-10-30 14:18:48 +0100

    heute

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,10 @@
 import System.Environment
 import System.Posix.Pty
-
+import System.Console.CmdArgs
+import Control.Monad (unless)
+import System.IO (isEOF)
+import System.Random
+        
 -- | Mainly main
 main :: IO ()
 main = getArgs >>= print . haqify . head
