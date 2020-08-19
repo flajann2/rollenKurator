1e20c04bfef02137c3ab8b4ca7ea92746b6cd763
Fred Mitchell <fred.mitchell@gmx.de>
2020-08-19 09:11:31 +0200

    mothballed

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,18 @@
+{- |
+Module      :  $Header$ 
+Description :  Main for rollenKurator
+Copyright   :  (c) 2017 Fred Mitchell
+License     :  MIT
+
+Maintainer  :  fred.mitchell@gmx.de
+Stability   :  experimental
+Portability :  portable
+
+RollenKurator takes multiple lines -- presumably from stdin -- to execute
+concurrently, with the ability to filter and display the output streams
+in your console / terminal as adjustable panes.
+-}
+
 import System.Environment
 import System.Posix.Pty
 import System.Console.CmdArgs
